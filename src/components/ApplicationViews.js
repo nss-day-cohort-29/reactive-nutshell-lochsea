6e4bc20ebfc25107eefa0302815d25e70c7f70b1
Hannah Neal <hannahmneal@gmail.com>
2019-01-28 15:48:12 -0600

    set up state for tasks, componentDidMount, and fetch call; no functionality yet

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,39 @@
 import { Route, Redirect } from "react-router-dom";
 import React, { Component } from "react";
+// Hannah: added import to pass props to task components:
+import TaskManager from "./todo/TaskManager";
+
 
 export default class ApplicationViews extends Component {
+
+
+state = {
+  tasks: []
+}
+
+//=============================================   Tasks Hook  ===============================================================
+
+// componentDidMount() {
+//     const newState = {}
+
+// Example code. Make this fit into how you have written yours.
+// AnimalManager.getAll().then(allAnimals => {
+//     this.setState({
+//         animals: allAnimals
+//     })
+// })
+
+componentDidMount() {
+  const newTaskState = {
+    TaskManager.getAll()
+    .then(allTasks => {
+      this.setState({
+        tasks: allTasks
+      })
+    })
+  }
+}
+//============================================================================================================
 
   render() {
     return (
@@ -30,17 +62,15 @@ export default class ApplicationViews extends Component {
 
         <Route
           path="/tasks" render={props => {
-            return null
-            // Remove null and return the component which will show the user's tasks
+            return <TaskList tasks={this.state.tasks} />
           }}
         />
-
         <Route
         exact path="/events" render={props => {
             return null
             // Remove null and return the component which will show events
           }}
-          />
+        />
       </React.Fragment>
     );
   }
