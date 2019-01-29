53a8abd030d28237d4141bed62b60c0ce69f762a
Hannah Neal <hannahmneal@gmail.com>
2019-01-29 13:23:46 -0600

    resolved nomenclature issues in tasklist

--- src/components/todo/TaskList.js
+++ src/components/todo/TaskList.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 
 export default class TaskList extends Component {
     render() {
-        console.log(this.props.todos)
+        // console.log(this.props.todos)
         return (
-            <section className="tasks">
-            <h5>
-            Tasks
-            </h5>
+            <section className="todos">
+            <h5>Tasks</h5> {
+                this.props.todos.map(errands =>
+                <div key={errands.id}>
+                {errands.name}
+                </div>)
+            }
             </section>
         )
     }
