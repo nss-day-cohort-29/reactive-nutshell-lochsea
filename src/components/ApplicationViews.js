b6b3ddea31dfa23f19dcdc518ab35d14156fba3b
selam <selam0315@gmail.com>
2019-01-29 13:05:02 -0600

    route input and edit form

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,16 +1,58 @@
-import { Route, Redirect } from "react-router-dom";
-import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom"
+import React, { Component } from "react"
+import NewsList from "./news/NewsList"
+import NewsForm from "./news/NewsForm"
+import NewsEditForm from "./news/NewsEditForm"
+import NewsManager from "./news/NewsManager"
 
 export default class ApplicationViews extends Component {
+  state = {
+    news: []
+   }
+   addNews = (article) =>{
+    NewsManager.post(article)
+      .then(() => NewsManager.getAll())
+      .then(news =>
+        this.setState({
+          news: news
+        })
+      )
+      }
 
+      updateNews = (newsId, editedNewsObj) => {
+        return NewsManager.put(newsId, editedNewsObj)
+        .then(() => NewsManager.getAll())
+        .then(news => {
+          this.setState({
+            news: news
+          })
+        });
+      }
+
+      componentDidMount() {
+        NewsManager.getAll().then(allNews => {
+          this.setState({
+            news: allNews
+          })
+        })
+      }
   render() {
     return (
       <React.Fragment>
 
         <Route
           exact path="/" render={props => {
-            return null
-            // Remove null and return the component which will show news articles
+            return ( <NewsList  news={this.state.news} /> )
+          }}
+        />
+        <Route
+          path="/new" render={props => {
+            return ( <NewsForm {...props} updateNews={this.updateNews} news={this.state.news} /> )
+          }}
+        />
+        <Route
+          path="/news/:newsId(\d+)/edit" render={props => {
+            return <NewsEditForm {...props} updateNews={this.updateNews}/>
           }}
         />
 
