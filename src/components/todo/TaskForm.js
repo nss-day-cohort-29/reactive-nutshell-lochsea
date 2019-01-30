2cd08848787d5aec47bd984b23435909d6b232a1
Hannah Neal <hannahmneal@gmail.com>
2019-01-30 11:12:38 -0600

    began form setup

---
+++ src/components/todo/TaskForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react"
+
+export default class TaskForm extends Component {
+
+    state = {
+        taskName: "",
+        task: ""
+    };
+
+    // Create a new task object:
+    createNewTask = evt => {
+        evt.preventDefault();
+        const newTask = {
+            name: this.state.taskName,
+            task: this.state.task,
+            //Connect the right task with the right id:
+            taskId: this.props.task.find(
+                task => task.taskName === this.state.task).id
+        };
+
+        this.props.addTask(newTask)
+        .then(() => this.props.history.push("/tasks"));
+    }
+
+render() {
+    return(
+    // create form elements
+    <React.Fragment>
+        <form className="task-form">
+            <div>
+                <label htmlFor="taskName">
+                    Task Name
+                </label>
+                <input type="text" required className="task-form-input" id="taskName" placeholder="Task Name"/>
+            </div>
+            <div>
+                <label htmlFor="taskDescription">Task Description</label>
+                <input type="text" required className="task-form-input" id="taskDesc" placeholder="Describe Task"/>
+            </div>
+            <button>Submit</button>
+        </form>
+    </React.Fragment>
+    )
+}
+}
